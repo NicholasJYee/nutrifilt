8c332e2d39c7ec01bdb119a04ba6349522ddbf8e
Nick Yee <njsyee@gmail.com>
2016-02-19 13:07:18 -0700

    Added Recipe, Ingredient, HealthLabel, and IngredientRecipe tables and populated table with 1 recipe: Amalfi salad.

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Recipe, Ingredient, HealthLabel, IngredientRecipe
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(HealthLabel)
+admin.site.register(IngredientRecipe)
--- meals/models.py
+++ meals/models.py
@@ -3,3 +3,72 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+  label = models.CharField(max_length=100)
+  image = models.CharField(max_length=512)
+  url = models.CharField(max_length=512)
+  servings = models.FloatField()
+  calories = models.FloatField()
+  fat = models.FloatField()
+  sat_fat = models.FloatField()
+  trans_fat = models.FloatField()
+  mono_unsat_fat = models.FloatField()
+  poly_unsat_fat = models.FloatField()
+  carbohydrates = models.FloatField()
+  fiber = models.FloatField()
+  sugar = models.FloatField()
+  protein = models.FloatField()
+  cholesterol = models.FloatField()
+  sodium = models.FloatField()
+  calcium = models.FloatField()
+  magnesium = models.FloatField()
+  potassium = models.FloatField()
+  iron = models.FloatField()
+  zinc = models.FloatField()
+  phosphorus = models.FloatField()
+  vit_a = models.FloatField()
+  vit_c = models.FloatField()
+  thiamin = models.FloatField()
+  riboflavin = models.FloatField()
+  niacin = models.FloatField()
+  vit_b6 = models.FloatField()
+  folic_acid = models.FloatField()
+  vit_b12 = models.FloatField()
+  vit_d = models.FloatField()
+  vit_e = models.FloatField()
+  vit_k = models.FloatField()
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+        return self.label
+
+class Ingredient(models.Model):
+  food = models.CharField(max_length=100)
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+        return self.food
+
+class HealthLabel(models.Model):
+  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+  label = models.CharField(max_length=150)
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+        return self.label
+
+class IngredientRecipe(models.Model):
+  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+  incredient = models.ForeignKey(Ingredient)
+  text = models.CharField(max_length=150)
+  food = models.CharField(max_length=100)
+  weight = models.FloatField()
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+        return self.text
