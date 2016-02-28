218fbd68059e714abc422141d229f93d47a81add
Nick Yee <njsyee@gmail.com>
2016-02-27 19:18:06 -0700

    Fortran nutrition met returns true or false (hard coded)

--- meals/simulated_annealing.f90
+++ meals/simulated_annealing.f90
@@ -10,28 +10,43 @@ SUBROUTINE generate_plan_meeting_nutrition(&
   REAL(8), DIMENSION(lunch_size, 31), INTENT(IN) :: lunch
   REAL(8), DIMENSION(dinner_size, 31), INTENT(IN) :: dinner
   REAL(8), DIMENSION(plan_size, 31), INTENT(INOUT) :: plan
-  INTEGER :: i, meal_number
+  INTEGER :: i, j, meal_number
+  LOGICAL :: met_nutrient_requirement, nutrition_met
 
+  DO i = 1, MAX_NUMB_OF_INITIAL_MEAL_PLAN_GENERATED
+    DO j = 1, plan_size
+      meal_number = INT(RAND(0) * breakfast_size) + 1
+      SELECT CASE (INT(plan(j,1)))
+        CASE (1)
+          plan(j,:) = breakfast(meal_number,:)
+        CASE (2)
+          plan(j,:) = snack(meal_number,:)
+        CASE (3)
+          plan(j,:) = lunch(meal_number,:)
+        CASE (4)
+          plan(j,:) = dinner(meal_number,:)
+      END SELECT
+    END DO
 
-  DO i = 1, plan_size
-    meal_number = INT(RAND(0) * breakfast_size) + 1
-    SELECT CASE (INT(plan(i,1)))
-      CASE (1)
-        plan(i,:) = breakfast(meal_number,:)
-      CASE (2)
-        plan(i,:) = snack(meal_number,:)
-      CASE (3)
-        plan(i,:) = lunch(meal_number,:)
-      CASE (4)
-        plan(i,:) = dinner(meal_number,:)
-    END SELECT
+    met_nutrient_requirement = nutrition_met(plan, nutrition_req)
+    IF (met_nutrient_requirement) THEN
+      plan = 1.d0
+    ELSE 
+      plan = 2.d0
+    END IF
   END DO
-  
-
-
-
 
 END SUBROUTINE generate_plan_meeting_nutrition
+
+LOGICAL FUNCTION nutrition_met(plan, plan_size, nutrition_req, nutrition_req_size)
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: plan_size, nutrition_req_size
+  REAL(8), DIMENSION(nutrition_req_size), INTENT(IN) :: nutrition_req
+  REAL(8), DIMENSION(plan_size, 31), INTENT(IN) :: plan
+
+  nutrition_met = .FALSE.
+
+END FUNCTION
 
 SUBROUTINE get_array(arr, row, col)
   IMPLICIT NONE
