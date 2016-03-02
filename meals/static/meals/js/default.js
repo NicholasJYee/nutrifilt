5442d16985a2e3dba7fb043a62e26ef5b0879ec8
hatimh <hatim@ualberta.ca>
2016-03-01 22:14:48 -0700

    Added smart search and all its features

--- meals/static/meals/js/default.js
+++ meals/static/meals/js/default.js
@@ -2,11 +2,12 @@ $(function() {
        
   $("#navBar ul li").on('click', function() {
     $(this).addClass('active').siblings('.active').removeClass('active');
-    var cdiv = '#'+ $(this).children('.navText').text().toLowerCase();     
+    var cdiv = '#'+ $(this).children('.navText').text().toLowerCase().replace(" ","");     
     $('.contents').children(cdiv).show().siblings().hide(); 
   });
 
   $('#loadForm').popup({ transition: 'all 0.3s'});
+  $('#loadForm2').popup({ transition: 'all 0.3s'});
 
   $('#search').on('click', function(){
     $('#loadForm').load('/meals/search/', function() {
@@ -39,7 +40,57 @@ $(function() {
     });      
   });
 
-  $('body').on('submit', '#daForm',function(){   
+   $('#search2').on('click', function(){
+    $('#loadForm2').load('/meals/search2/', function() {
+      $('#loadForm2 input:lt(6)').attr('required',true);
+      $('#loadForm2 p').slice(6).hide(); 
+      var select_for_num_meals = $('select[name="num_of_meals"]');
+      meal_dropdown(select_for_num_meals);
+
+      var rangeValues = {
+        "1": "Meal cost: Cheapest",
+        "5.5": "Meal cost: Cheap",
+        "10": "Meal cost: Normal"
+      };
+
+      var rangeInput = $('#rangeInput');
+      var rangeText = $('#rangeText');
+      rangeText.text(rangeValues[rangeInput.val()]);
+      rangeInput.on('input change', function () {
+        rangeText.text(rangeValues[$(this).val()]);
+        console.log(rangeValues[$(this).val()])
+      });
+
+      select_for_num_meals.change(function() {
+        $('.meal-type').remove();
+        meal_dropdown(select_for_num_meals);
+      });
+
+    });      
+  });
+
+  $('body').on('click', '#smart', function(){
+    var cal = ($("#weight").val()*13.4) + ($("#height").val()*6.25) - ($("#age").val()*5);    
+    if ($("#gender").val() == "1") {
+      cal += 5;
+      
+    }else {
+      cal -= 163;     
+    }
+    if ($("#planType").val() == "2") { cal -= 500};
+
+    if ($("#planType").val() == "3") { cal = cal * 1.3};
+    $("#id_calories").val(cal);
+    $("#id_fat").val(cal*65/2000);
+    $("#id_carbohydrates").val(cal*300/2000);
+    if($("#planType").val() == "3") { 
+      $("#id_protein").val(cal*50*1.3/2000);      
+    } else {
+      $("#id_protein").val(cal*50/2000);
+    };    
+  }); 
+
+  $('body').on('submit', '.daForm',function(){   
     $('body').load("/meals/game");     
   }); 
 
@@ -94,7 +145,7 @@ var meal_dropdown = function(select_for_num_meals) {
       "  <option value='3'>Lunch</option>\n" +
       "  <option selected='selected' value='4'>Dinner</option>\n" +
       "</select></p>";
-    $('#daForm > select').after(meal_dropdown_html);          
+    $('.daForm > select').after(meal_dropdown_html);          
   } else {
     for (var i = select_for_num_meals.val() - 1; i >= 0; i--) {
       var meal_dropdown_html =
@@ -105,7 +156,7 @@ var meal_dropdown = function(select_for_num_meals) {
         "  <option value='3'>Lunch</option>\n" +
         "  <option value='4'>Dinner</option>\n" +
         "</select>";
-      $('#daForm > select').after("<p class='meal-type'>" + meal_dropdown_html + "</p>");
+      $('.daForm > select').after("<p class='meal-type'>" + meal_dropdown_html + "</p>");
     }
   }
 };
