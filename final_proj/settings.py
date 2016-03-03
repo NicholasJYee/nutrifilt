40d94028ff65034416486eb24b78009e6c58ed0a
Nick Yee <njsyee@gmail.com>
2016-03-03 09:24:39 -0700

    Moved static files to final_proj folder again

--- final_proj/settings.py
+++ final_proj/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../meals/static'),
+)
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
